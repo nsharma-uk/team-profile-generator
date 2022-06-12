ff27af22c4f2fda2ad0cad779867d0f95910f5c7
nsharma-uk <n.sharmauk711@gmail.com>
2022-06-12 15:41:35 +0100

    employee test and employee.js updated

--- src/lib/employee.test.js
+++ src/lib/employee.test.js
@@ -2,18 +2,40 @@
 const Employee = require('./employee');
 
 // should be an instance of an Employee
+const employee = new Employee({
+    name: "nayan",
+    id: "123",
+    email: "nayan@email.com",
+  });
+
+
+    
+// - should return the expected role
 describe ("Employee", () => {
-it("should return the name Nayan", () => {
+it ("role of employee", () => {
+const expected = "Employee";
+const actual = employee.getRole();
+expect(actual).toEqual(expected);
+});
 
-const employee = new Employee ("Nayan")
-const actual = employee.getName ()
-expect (actual).toEqual("Nayan")
-
-}
-
-
-}
 // - should return the expected name
+it("should return the expected name", () => {
+const expected = "Nayan";
+const actual = employee.getName();
+expect(actual).toEqual(expected);
+});
+
 // - should return the expected id
+it("should return the expected id", () => {
+const expected = "123";
+const actual = employee.getId();
+expect(actual).toEqual(expected);
+});
+
 // - should return the expected email
-// - should return the role of Employee
+it("should get the email of the employee", () => {
+const expected = "nayan";
+const actual = employee.getEmail();
+expect(actual).toEqual(expected);
+});
+});
